715e239b1cb8d6ba84e9a255ecadca766b41de7c
apozhidaev <alexander.pozhidaev@gmail.com>
2015-06-30 02:55:16 +0300

    implemented base architecture

--- AlexanderPo/Controllers/AppController.cs
+++ AlexanderPo/Controllers/AppController.cs
@@ -15,7 +15,7 @@ namespace AlexanderPo.Controllers
 #if DEBUG
         private static readonly string Version = Guid.NewGuid().ToString();
 #else
-        private const string Version = "0.1.171";
+        private const string Version = "0.1.4";
 #endif
 
         public AppController(IServiceManager serviceManager) 
