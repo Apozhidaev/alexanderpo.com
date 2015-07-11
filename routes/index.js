5c24b9043257115a302877eaeca5a8edd6213e37
apozhidaev <alexander.pozhidaev@gmail.com>
2015-07-11 20:26:51 +0300

    hotfix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 
-/*
- * GET home page.
- */
+var path = require('path');
+var fs = require('fs')
 
 exports.index = function(req, res){
   res.redirect('index.html');
+};
+
+exports.appcache = function(req, res){
+
+  fs.readFile(path.join(__dirname, '../cache.appcache'), 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    res.contentType('text/cache-manifest');
+    res.send(200, data)
+  });
+
 };
