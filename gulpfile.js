a8e3f16dea526e3e1d9135b2f20818cc82e03679
alexanderpo <alexander.pozhidaev@gmail.com>
2017-01-12 14:48:27 +0300

    refactoring

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+﻿var gulp = require("gulp");
+var insert = require('gulp-insert');
+var handlebars = require('handlebars');
+var rimraf = require("rimraf");
+var fs = require('fs');
+
+var paths = {
+    build: './build/',
+    src: './src/'
+};
+
+var titles = {
+    "links.html": "links - alexander .pozhidaev"
+};
+
+gulp.task('clean', function (cb) {
+  rimraf(paths.build, cb);
+});
+
+gulp.task('copy', ['clean'], function () {
+
+    return gulp.src(['!' + paths.src + '**/*.{html,hbs}',
+            paths.src + '**/*.*'])
+        .pipe(gulp.dest(paths.build));
+
+});
+
+gulp.task('build', ['copy'], function () {
+
+    var layout = fs.readFileSync(paths.src + 'layout.hbs', 'utf8');
+    var template = handlebars.compile(layout);
+
+    return gulp.src(paths.src + '*.html')
+        .pipe(insert.transform(function(contents, file) {
+            var context = {title: titles[file.relative] || 'alexander .pozhidaev'};
+            context.body = handlebars.compile(contents)(context);
+            return template(context);
+        }))
+        .pipe(gulp.dest(paths.build));
+
+
+});
